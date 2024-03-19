217884d10bf26d98dd4cc578e58e1c24e84cb334
gigigar <133097398+gigigar@users.noreply.github.com>
2024-03-20 02:29:28 +0800

    restricted recipe details

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
 from .models import Recipe
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
 
-class RecipeListView(ListView):
+class RecipeListView(LoginRequiredMixin, ListView):
     model = Recipe
     template_name = 'ledger/recipe_list.html'
 
-class RecipeDetailsView(DetailView):
+class RecipeDetailsView(LoginRequiredMixin, DetailView):
     model = Recipe
     template_name = 'ledger/recipe_details.html'
