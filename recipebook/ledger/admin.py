9f7b53aa1663599bebb4b624f67e8cc383dcd4c5
gigigar <133097398+gigigar@users.noreply.github.com>
2024-03-19 17:07:58 +0800

    edited admin.py to accomodate Profile model

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import Recipe, RecipeIngredient, Ingredient
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+
+from .models import Profile
 
 # Register your models here.
 
@@ -7,12 +11,19 @@ class RecipeIngredientInline(admin.TabularInline):
     model = RecipeIngredient
     
 class RecipeAdmin(admin.ModelAdmin):
-    inlines = [
-        RecipeIngredientInline,
-    ]
+    inlines = [RecipeIngredientInline,]
 
 class IngredientAdmin(admin.ModelAdmin):
     list_display = ['name']
+    
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    
+class UserAdmin(admin.BaseUserAdmin):
+    inlines = [ProfileInlin,]
 
 admin.site.register(Ingredient, IngredientAdmin)
-admin.site.register(Recipe, RecipeAdmin)    
+admin.site.register(Recipe, RecipeAdmin) 
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)   
